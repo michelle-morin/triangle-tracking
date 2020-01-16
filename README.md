9493b3fa5800e978ddb02b4b60a6655375a2f04f
Michelle Morin and Rami Ahmed <michelle@>
2020-01-16 14:34:31 -0800

    update README and add logic for handling non-numerical inputs

---
+++ README.md
@@ -0,0 +1,25 @@
+# _Triangle Tracker_
+
+#### _jQuery project for Epicodus_, _16 January 2020_
+
+#### By _**Michelle Morin, Rami Ahmed**_
+
+## Description
+
+_This webpage includes a form that accepts input of three numbers and outputs whether the lengths form a scalene, isosceles, equilateral triangle, or whether they do not form a triangle._
+
+## Setup/Installation Requirements
+
+* _First, clone this repository to your desktop by navigating to the desktop in the Terminal (entering "cd desktop" in the Terminal) and then entering "git clone" followed by the link to this repository._
+* _Once the directory is cloned to your desktop, open the directory in Visual Studio Code by entering the command "code ." in the Terminal._
+* _Open index.html in a browser of your choice._
+
+## Technologies Used
+
+_Git, HTML, CSS, JS, jQuery_
+
+### License
+
+*This webpage is licensed under the MIT license.*
+
+Copyright (c) 2020 **_Michelle Morin, Rami Ahmed_**
--- js/scripts.js
+++ js/scripts.js
@@ -11,21 +11,25 @@ $(document).ready(function() {
     var secondSide = parseInt($("input#side2").val());
     var thirdSide = parseInt($("input#side3").val());
     
-    if (firstSide + secondSide <= thirdSide || secondSide + thirdSide <= firstSide || firstSide + thirdSide <= secondSide) {
+    if (isNaN(firstSide) || isNaN(secondSide) || isNaN(thirdSide)) {
+      alert("Please enter all numerical inputs")
+    } else if (firstSide + secondSide <= thirdSide || secondSide + thirdSide <= firstSide || firstSide + thirdSide <= secondSide) {
       $(".not").text("not");
       $(".triangle").text("");
+      $("#output").show();
     } else if (firstSide === secondSide && firstSide === thirdSide) {
       $(".not").text("");
       $(".triangle").text("equilateral");
+      $("#output").show();
     } else if ((firstSide === secondSide && secondSide != thirdSide) || (secondSide === thirdSide && secondSide != firstSide)) {
       $(".not").text("");
       $(".triangle").text("isosceles");
+      $("#output").show();
     } else if (firstSide != secondSide && firstSide != thirdSide && secondSide != thirdSide) {
       $(".not").text("");
       $(".triangle").text("scalene");
+      $("#output").show();
     }
-    
-    $("#output").show();
   });
 });
 
